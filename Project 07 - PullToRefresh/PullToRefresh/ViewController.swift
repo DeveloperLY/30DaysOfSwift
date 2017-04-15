d1eb4db17306e3b443e3cb25901ddd3f1e553899
LY <coderyliu@gmail.com>
2017-04-15 23:24:33 +0800

    Swift 3+ Version

--- Project 07 - PullToRefresh/PullToRefresh/ViewController.swift
+++ Project 07 - PullToRefresh/PullToRefresh/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  PullToRefresh <https://github.com/CoderYLiu/30DaysOfSwift>
+//  PullToRefresh <https://github.com/DeveloperLY/30DaysOfSwift>
 //
 //  Created by Liu Y on 16/4/13.
-//  Copyright © 2016年 CoderYLiu. All rights reserved.
+//  Copyright © 2016年 DeveloperLY. All rights reserved.
 //
 //  This source code is licensed under the MIT-style license found in the
 //  LICENSE file in the root directory of this source tree.
@@ -25,48 +25,48 @@ class ViewController: UITableViewController {
         emojiData = favoriteEmoji
         
         self.tableView.backgroundColor = UIColor(red:0.092, green:0.096, blue:0.116, alpha:1)
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifer)
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifer)
         
         self.refreshControl = UIRefreshControl()
-        self.refreshControl!.addTarget(self, action: "didRoadEmoji", forControlEvents: .ValueChanged)
+        self.refreshControl!.addTarget(self, action: #selector(ViewController.didRoadEmoji), for: .valueChanged)
         
         self.refreshControl!.backgroundColor = UIColor(red:0.113, green:0.113, blue:0.145, alpha:1)
-        let attributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        self.refreshControl!.attributedTitle = NSAttributedString(string: "Last updated on \(NSDate())", attributes: attributes)
-        self.refreshControl!.tintColor = UIColor.whiteColor()
+        let attributes = [NSForegroundColorAttributeName: UIColor.white]
+        self.refreshControl!.attributedTitle = NSAttributedString(string: "Last updated on \(Date())", attributes: attributes)
+        self.refreshControl!.tintColor = UIColor.white
         
         self.title = "emoji"
-        self.navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.blackTranslucent
         
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 60.0
-        self.tableView.tableFooterView = UIView(frame: CGRectZero)
-        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.none
         
     }
     
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.LightContent
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
     }
 
     // MARK: - UITableViewDataSource
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return emojiData.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifer)! as UITableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifer)! as UITableViewCell
         
         cell.textLabel?.text = self.emojiData[indexPath.row];
         
-        cell.textLabel?.textAlignment = NSTextAlignment.Center
+        cell.textLabel?.textAlignment = NSTextAlignment.center
         
-        cell.textLabel?.font = UIFont.systemFontOfSize(50.0)
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 50.0)
         
-        cell.backgroundColor = UIColor.clearColor()
+        cell.backgroundColor = UIColor.clear
         
-        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
         
         return cell
     }
