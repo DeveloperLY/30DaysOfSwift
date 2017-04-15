517282442b772e005b03e22a7b3bed7fc6dbb842
LY <coderyliu@gmail.com>
2017-04-16 02:33:03 +0800

    Swift 3+ Version

--- Project 19 - CustomPullToRefresh/PullRefreshTests/PullRefreshTests.swift
+++ Project 19 - CustomPullToRefresh/PullRefreshTests/PullRefreshTests.swift
@@ -1,9 +1,9 @@
 //
 //  PullRefreshTests.swift
-//  PullRefreshTests <https://github.com/CoderYLiu/30DaysOfSwift>
+//  PullRefreshTests <https://github.com/DeveloperLY/30DaysOfSwift>
 //
 //  Created by Liu Y on 16/4/25.
-//  Copyright © 2016年 CoderYLiu. All rights reserved.
+//  Copyright © 2016年 DeveloperLY. All rights reserved.
 //
 //  This source code is licensed under the MIT-style license found in the
 //  LICENSE file in the root directory of this source tree.
@@ -31,7 +31,7 @@ class PullRefreshTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
